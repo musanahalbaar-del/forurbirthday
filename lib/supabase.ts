83c0e26ba90db1ea1b1fa47b4aea91c34527d88a
Hasan Almusanna <musanah.albaar@gmail.com>
2026-04-15 15:41:21 +0700

    fix supabase key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://lhvwhetjipooftmabpsy.supabase.co";
-const supabaseAnonKey = "ISI_PUBLISHABLE_KEY_KAMU";
+const supabaseAnonKey = "sb_publishable_oFLocnd_4NmWKNCzZfNd1A_5HrxXF6W";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
